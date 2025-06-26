af02f993af5f2e2b860697dfe3fec4c0c1677513
Javier <javierperezviloria07@gmail.com>
2025-06-25 21:29:13 -0500

    feat(doc): add sphinx_rtd_theme for improved documentation appearance
    - Installed and configured the Read the Docs theme for Sphinx documentation
    - Updated conf.py to use sphinx_rtd_theme
    - Documentation now has a modern and user-friendly interface

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Inventory CRUD"
-copyright = "2025, Javi-CD"
-author = "Javi-CD"
+copyright = "2025, Javier Pérez"
+author = "Javi-CDx"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -17,6 +17,11 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
+
+
+def setup(app):
+    app.add_css_file("custom.css")
+
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -31,5 +36,12 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "light_logo": "",
+    "dark_logo": "",
+    "sidebar_hide_name": True,
+    "navigation_with_keys": True,
+}
